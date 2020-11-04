b4a748e2fcccff6653fff7e4538b6e66380d3eb4
Kun <extrakun@yahoo.com.sg>
2020-11-04 04:50:13 +0000

    Using another method

--- index.js
+++ index.js
@@ -1,6 +1,21 @@
-var readline = require('readline-sync');
-n = parseInt(readline.question());
-m = parseInt(readline.question());
-console.log(n+m)
-// made a change to force auto-testing
-// force commit
+const readline = require('readline');
+
+const rl = readline.createInterface({ input: process.stdin , output: process.stdout });
+
+const getLine = (function () {
+    const getLineGen = (async function* () {
+        for await (const line of rl) {
+            yield line;
+        }
+    })();
+    return async () => ((await getLineGen.next()).value);
+})();
+
+const main = async () => {
+    let a = Number(await getLine());
+    let b = Number(await getLine());
+    console.log(a+b);
+    process.exit(0);
+};
+
+main();
